f0b7b73ded48426c3210f21ed7b0cd213bdd2f9b
Allah <mail@maial.pl>
2022-11-04 11:03:43 +0100

    string.js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// /*
+//     Metody do wykorzystania:
+//     charAt()
+//     includes()
+//     replace()* podchwytliwe 🙂 
+//     slice()
+//     split()
+//     toLowerCase()
+//     toUpperCase()
+// */
+ 
+// const text1 = 'powiększ mnie!'
+// const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI'
+// const text3 = '$#%#^ wytnij te dziwne znaki na początku!'
+// const text4 = 'sprawdź, czy zawieram słowo "czy"'
+// const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"'
+// const text6 = 'pies zamień każde słowo pies, na słowo kot pies'
+// const text7 = 'podziel, ten, string, od, przecinków'
+
+// const textToReplace = /pies/ig
+// const words = text7.split(', ')
+
+// console.log(text1.toUpperCase());
+// console.log(text2.toLowerCase());
+// console.log(text3.slice(6));
+// console.log(text4.includes('czy'));
+// console.log(text5.charAt(text5.length-6).toUpperCase());
+// console.log(text6.replaceAll(textToReplace, 'kot'));
+// console.log(words);
+
+
