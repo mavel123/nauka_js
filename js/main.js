e3fb22c97743618507e7a14a41ea526b6fb838b7
Allah <mail@maial.pl>
2022-11-06 21:06:30 +0100

    arrow rotate

--- js/main.js
+++ js/main.js
@@ -139,22 +139,203 @@
 // const fahrenheit = (item) => {
 //     return result = item*1,8+32
 // }
-// temp = fahrenheit(celsius=10)
+// // temp = fahrenheit(celsius=10)
 
-// console.log( `${celsius}*C =  ${temp}*F`)
+// // console.log( `${celsius}*C =  ${temp}*F`)
 
-let num = 10
-const numbers = []
-for (let i = 0; i <= num; i++) {
-	numbers.push(i)
+// let num = 10
+// const numbers = []
+// for (let i = 0; i <= num; i++) {
+// 	numbers.push(i)
+// }
+
+// console.log(numbers)
+
+// const divideByThree = item => {
+// 	item % 3 === 0 ? console.log(`${item} jest podzielny przez 3`) : console.log(`${item} nie jest podzielny przez 3`)
+// }
+
+// console.log(numbers.forEach(divideByThree))
+
+// //apendchild()
+// const heading = document.querySelector('h1')
+// const paragraph = document.querySelectorAll('p')
+
+// const divTest = document.querySelector('div.test')
+// const paraDiv = divTest.querySelector(' p#test')
+
+// const ulList = document.createElement('ul')
+// const liItem = document.createElement('li')
+
+// liItem.textContent='czesc uzytkowniku'
+
+// document.body.appendChild(ulList)
+// ulList.appendChild(liItem)
+
+// const divTest2 =document.querySelector('div.test2')
+
+// divTest2.appendChild(document.createElement('p'))
+
+// const div = document.querySelector('div')
+
+// const headingTwo = document.createElement('h2')
+// headingTwo.textContent= 'jestem naglowek 2'
+// div.appendChild(headingTwo)
+
+
+// ///apend()
+
+// div.append(document.createElement('p'), headingTwo)
+
+//innerHTML, outerHTML, textContent, innerText 80 odc. 1czjs
+
+
+// const input = document.querySelector('input')
+// const underInput = document.querySelector('.under-input')
+
+// input.addEventListener('keydown', e => {
+//     if(e.key === 'Enter'){
+//         // <h2 style="color: red;"></h2>
+//         underInput.innerHTML = input.value
+//         // underInput.textContent = input.value
+//     }
+// })
+
+// const ulList= document.createElement('ul')
+// const body = document.querySelector('body')
+
+// body.append(ulList)
+
+
+// for(let j= 1; j<=10; j++ )
+// {
+// const liItem= document.createElement('li')
+// ulList.append(liItem)
+// liItem.textContent = j
+// }
+
+//  ulList.lastChild.classList.add('last-li')
+
+
+// const square = document.querySelector('.square')
+// const paragraph = document.querySelectorAll('p')
+// const burgerBtn = document.querySelector('.btn1')
+// const pizzaBtn = document.querySelector('.btn2')
+
+// const textHello = () => {
+//     console.log('hello')
+// }
+// const redColor = () => {
+//     square.classList.add('red')
+//     square.classList.remove('blue')
+// }
+// const blueColor = () => {
+//     square.classList.toggle('blue')
+//     square.classList.remove('red')
+// }
+
+// const addShow = () => {
+//     paragraph.forEach(item => item.classList.toggle('show'))
+// }
+
+
+// burgerBtn.addEventListener('dblclick', textHello)
+// square.addEventListener('mouseover', redColor)
+// square.addEventListener('mouseout', blueColor)
+// pizzaBtn.addEventListener('click', addShow)
+
+// setTimeout(addShow, 1000)
+
+// const colors = ['red', 'green', 'blue']
+// alert('czesc')
+// confirm('ja tuu tetraz?')
+
+
+// const image = document.querySelector('img')
+
+// image.setAttribute('src', 'https://cdn.pixabay.com/photo/2014/09/05/21/55/astra-436593_960_720.jpg')
+// image.setAttribute('alt', 'chabry')
+
+// const texting = () => {
+//     console.log('sadsadsadsadsada')
+// }
+// setTimeout(texting, 2000)
+
+// const liItems = document.querySelectorAll('li')
+
+// for (let i=1; i<=5; i++ ){
+//     liItems[i-1].textContent = i
+// }
+
+// liItems.forEach((item, index) => {
+//     item.textContent = index+1
+// });
+
+
+
+// const thirdLi =document.querySelector('[data-id="3"]')
+// console.log(thirdLi)
+// console.log(thirdLi.closest('.wrapper'))
+
+
+
+
+
+
+// const newDiv = document.createElement('div')
+// newDiv.classList.add('circle', 'gold' )
+// lime.appendChild(newDiv)
+
+
+// const circles = document.querySelectorAll('.circle')
+
+// circles.forEach(circle => circle.addEventListener('click', () => {
+//     console.log(circle)  
+//   } ))
+
+// console.log('czesc');
+// console.log(window.console);
+
+// const persons = [
+//     {
+//         name: 'lisa',
+//         age: 24
+//     },
+//     {
+//         name: 'andrzej',
+//         age: 54
+//     }
+// ]
+
+// console.table(persons)
+
+
+
+
+
+
+
+
+
+
+
+
+// btn2.addEventListener('mouseover', () => {
+//     console.log('najechano na mnie')
+// })
+// btn3.addEventListener('dblclick', test)
+
+const arrowBtn = document.querySelector('.arrow')
+const arrowIcon = document.querySelector('.fas')
+const img = document.querySelector('.item1')
+
+//nasluchiwanie na click na btn
+//dodajemy klase height
+//rotate na fas
+
+arrowBtn.addEventListener('click', ()=>{
+    img.classList.toggle('hide')
+    arrowIcon.style.rotate = '180deg'
+    img.classList.contains('hide') ? arrowIcon.style.rotate = '180deg' : arrowIcon.style.rotate = '0deg' 
 }
-
-console.log(numbers)
-
-const divideByThree = item => {
-	item % 3 === 0 ? console.log(`${item} jest podzielny przez 3`) : console.log(`${item} nie jest podzielny przez 3`)
-}
-
-console.log(numbers.forEach(divideByThree))
-
-
+)
