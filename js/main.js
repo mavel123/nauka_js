b7ece5b691a7f17f346fd8193da2cbe71bd4eac6
Allah <mail@maial.pl>
2022-11-12 23:34:59 +0100

    done notes

--- js/main.js
+++ js/main.js
@@ -1,42 +1,120 @@
-const swapBtn = document.querySelector('.swap')
-const currencyOne = document.querySelector('#currency-one')
-const currencyTwo = document.querySelector('#currency-two')
-const amountOne = document.querySelector('.amount-one')
-const amountTwo = document.querySelector('.amount-two')
-const rateInfo = document.querySelector('.rate-info')
-const myHeaders = new Headers()
-myHeaders.append('apikey', 'ZwFVOGLqOmFDpxZpamqOgj3q0TJtB9FF')
+const incomeSection = document.querySelector('.income-area')
+const expenseSection = document.querySelector('.expenses-area')
+const availableMoney = document.querySelector('.available-money')
+const addTransactionPanel = document.querySelector('.add-transaction-panel')
+const nameInput = document.querySelector('#name')
+const amountInput = document.querySelector('#amount')
+const categorySelect = document.querySelector('#category')
 
-const requestOptions = {
-	method: 'GET',
-	redirect: 'follow',
-	headers: myHeaders,
+const addTransactionBtn = document.querySelector('.add-transaction')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+const deleteBtn = document.querySelector('.delete')
+const deleteAllBtn = document.querySelector('.delete-all')
+
+let root = document.documentElement;
+let ID = 0;
+let categoryIcon
+let selectedCategory
+let moneyArr = [0]
+const showPanel = () => {
+	addTransactionPanel.style.display = 'flex'
 }
-const calculate = () => {
-	fetch(
-		`https://api.apilayer.com/exchangerates_data/latest?symbols=${currencyTwo.value}&base=${currencyOne.value}`,
-		requestOptions
-	)
-		.then(response => response.json())
-		.then(result => {
-			const currency1 = currencyOne.value
-			const currency2 = currencyTwo.value
-			const rate = result.rates[currency2]
-			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(4)} ${currency2}`
-			amountTwo.value = (amountOne.value * rate).toFixed(2)
-		})
-		.catch(error => console.log('error', error))
+const closePanel = () => {
+	addTransactionPanel.style.display = 'none'
+	clearInputs()
 }
-const handleSwap = () => {
-	let bufor
-	bufor = currencyOne.value
-	currencyOne.value = currencyTwo.value
-	currencyTwo.value = bufor
-	calculate()
+const checkForm = () => {
+	
+	if(nameInput.value !== '' && amountInput.value != '' && categorySelect.value !== 'none'){
+		createNewTransaction()
+	}else{
+		alert('wypelnij wszystkie pola')
+	}
+}
+const clearInputs = () => {
+		nameInput.value = ''
+		amountInput.value = ''
+		categorySelect.value = 'none'
+}
+const createNewTransaction = () => {
+		const newTransaction = document.createElement('div')
+		newTransaction.classList.add('transaction')
+		newTransaction.setAttribute('id', ID)
+		checkCategory(selectedCategory)
+		
+		newTransaction.innerHTML =  `<p class="transaction-name">${categoryIcon} ${nameInput.value}</p>
+		<p class="transaction-amount">${amountInput.value} <button class="delete" onclick = deleteTransaction(${ID}) ><i class="fas fa-times"></i></button></p>`
+
+		amountInput.value > 0 ? (incomeSection.append(newTransaction), newTransaction.classList.add('income')) : 
+		(expenseSection.append(newTransaction), newTransaction.classList.add('expense'))
+
+		moneyArr.push(+amountInput.value)
+		closePanel();
+		ID++;
+		clearInputs()	
+		countMoney(moneyArr)
+}
+const selectCategory = () => {
+	selectedCategory = categorySelect.options[categorySelect.selectedIndex].text
+}
+const checkCategory = (transaction) => {
+	
+	switch(transaction){
+		case'[ + ] Przychód': categoryIcon= '<i class="fas fa-money-bill-wave"></i>'
+		break;
+		case'[ - ] Zakupy': categoryIcon ='<i class="fas fa-cart-arrow-down"></i>'
+		break;
+		case'[ - ] Jedzenie': categoryIcon ='<i class="fas fa-hamburger"></i> '
+		break;
+		case'[ - ] Kino': categoryIcon ='<i class="fas fa-film"></i> '
+		break;
+	}	
 }
 
-currencyOne.addEventListener('change', calculate)
-currencyTwo.addEventListener('change', calculate)
-amountOne.addEventListener('input', calculate)
-swapBtn.addEventListener('click', handleSwap)
-calculate()
+const deleteTransaction = (idToDel) => {
+	const transactionToDel = document.getElementById(idToDel)
+	console.log(transactionToDel.childNodes)
+	const transactionAmount = transactionToDel.childNodes[2].innerText
+	
+	const indexOfTransaction = moneyArr.indexOf(transactionAmount)
+	moneyArr.splice(indexOfTransaction,1)
+	transactionToDel.classList.contains('income') ? incomeSection.removeChild(transactionToDel) : 
+	expenseSection.removeChild(transactionToDel)
+	countMoney(moneyArr)
+}
+const deleteAllTransaction = () => {
+	incomeSection.innerHTML = '<h3>Przychód:</h3>'
+	expenseSection.innerHTML = '<h3>Wydatki:</h3>'
+	moneyArr = [0]
+	availableMoney.textContent = '0zł'
+}
+
+
+
+const countMoney = (money) => {
+	const newMoney = money.reduce((a,b)=> a+b)
+	availableMoney.textContent = newMoney
+}
+
+const changeStyleToLight = () => {
+	root.style.setProperty('--first-color', '#F9F9F9')
+	root.style.setProperty('--second-color', '#14161F')
+	root.style.setProperty('--border-color', 'rgba(0, 0, 0, .2)')
+	
+}
+const changeStyleToDark = () => {
+	root.style.setProperty('--first-color', '#14161F' )
+	root.style.setProperty('--second-color', '#F9F9F9')
+	root.style.setProperty('--border-color', 'rgba(255, 255, 255, .4)')
+	
+}
+
+const lightBtn = document.querySelector('.light')
+const darkBtn = document.querySelector('.dark')
+addTransactionBtn.addEventListener('click', showPanel)
+cancelBtn.addEventListener('click', closePanel)
+saveBtn.addEventListener('click', checkForm)
+deleteAllBtn.addEventListener('click', deleteAllTransaction)
+lightBtn.addEventListener('click', changeStyleToLight)
+darkBtn.addEventListener('click', changeStyleToDark)
