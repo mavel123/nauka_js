fe7f406adee0364346b2ddc293ac0777419d8872
Allah <mail@maial.pl>
2022-11-12 13:54:26 +0100

    doneCalculator

--- js/main.js
+++ js/main.js
@@ -1,84 +1,42 @@
-const addBtn = document.querySelector('.add')
-const saveBtn = document.querySelector('.save')
-const cancelBtn = document.querySelector('.cancel')
-const deleteBtns = document.getElementsByClassName('.delete')
-const deleteAllBtn = document.querySelector('.delete-all')
-const noteArea = document.querySelector('.note-area')
+const swapBtn = document.querySelector('.swap')
+const currencyOne = document.querySelector('#currency-one')
+const currencyTwo = document.querySelector('#currency-two')
+const amountOne = document.querySelector('.amount-one')
+const amountTwo = document.querySelector('.amount-two')
+const rateInfo = document.querySelector('.rate-info')
+const myHeaders = new Headers()
+myHeaders.append('apikey', 'ZwFVOGLqOmFDpxZpamqOgj3q0TJtB9FF')
 
-const notePanel = document.querySelector('.note-panel')
-const category = document.querySelector('#category')
-const textArea = document.querySelector('#text')	
-const error = document.querySelector('.error')
-let selectedValue;
-let cardID = 1;
+const requestOptions = {
+	method: 'GET',
+	redirect: 'follow',
+	headers: myHeaders,
+}
+const calculate = () => {
+	fetch(
+		`https://api.apilayer.com/exchangerates_data/latest?symbols=${currencyTwo.value}&base=${currencyOne.value}`,
+		requestOptions
+	)
+		.then(response => response.json())
+		.then(result => {
+			const currency1 = currencyOne.value
+			const currency2 = currencyTwo.value
+			const rate = result.rates[currency2]
+			rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(4)} ${currency2}`
+			amountTwo.value = (amountOne.value * rate).toFixed(2)
+		})
+		.catch(error => console.log('error', error))
+}
+const handleSwap = () => {
+	let bufor
+	bufor = currencyOne.value
+	currencyOne.value = currencyTwo.value
+	currencyTwo.value = bufor
+	calculate()
+}
 
-const openPanel = () => {
-	notePanel.style.display = 'flex'
-}
-const hidePanel = () => {
-	notePanel.style.display = 'none'
-	error.style.visibility = 'hidden'
-	textArea.value = ''
-	category.selectedIndex= 0
-}
-const addNote = () => {
-	if(textArea.value != '' && category.options[category.selectedIndex].value != 0){
-		error.style.visibility = 'hidden'
-		
-		createNote()
-	}else{
-		error.style.visibility = 'visible'
-	}
-}
-const createNote = () => {
-	const newNote = document.createElement('div')
-	newNote.classList.add('note')
-	newNote.setAttribute('id', cardID)
-	
-	newNote.innerHTML = `
-	<div class="note-header">
-	<h3 class="note-title">${selectedValue} #${cardID}</h3>
-	<button class="delete-note" onclick='deleteNote(${cardID})'>
-		<i class="fas fa-times icon"></i>
-	</button>
-	</div>
-	<div class="note-body">
-	${textArea.value}
-	</div>
-`	
-	cardID++
-	noteArea.append(newNote)
-	category.selectedIndex = 0 
-	textArea.value = ''
-	notePanel.style.display='none'
-	checkColor(newNote)
-}
-const selectValue = () => {
-	selectedValue = category.options[category.selectedIndex].text
-}
-const checkColor = note => {
-	switch(selectedValue){
-		case 'Zakupy':
-			note.style.backgroundColor = 'rgb(72,255,0)'
-			break;
-		case 'Praca':
-			note.style.backgroundColor = 'rgb(255,243,0)'
-			break;
-		case 'Inne':
-			note.style.backgroundColor = 'rgb(0,170,255)'
-			break;
-	}
-}
-const deleteNote = (id) => {
-	const noteToDelete = document.getElementById(id)
-	noteArea.removeChild(noteToDelete)
-	cardID--
-}
-const deleteAllNotes = () => {
-	noteArea.textContent = ''
-	cardID = 1
-}
-addBtn.addEventListener('click', openPanel)
-cancelBtn.addEventListener('click', hidePanel)
-saveBtn.addEventListener('click', addNote)
-deleteAllBtn.addEventListener('click', deleteAllNotes)
+currencyOne.addEventListener('change', calculate)
+currencyTwo.addEventListener('change', calculate)
+amountOne.addEventListener('input', calculate)
+swapBtn.addEventListener('click', handleSwap)
+calculate()
